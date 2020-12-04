6811832c7b86733919ad0e12ff407ee2fb162098
Henri Wihinen <henri.wihinen@solita.fi>
2020-12-04 15:57:37 +0200

    Day 4

---
+++ src/main/kotlin/day4/day4.kt
@@ -0,0 +1,79 @@
+package day4
+
+import java.io.File
+
+val part1fieldRegexes = listOf<Pair<String, Regex>>(
+    Pair("byr", ".*".toRegex()),
+    Pair("iyr", ".*".toRegex()),
+    Pair("eyr", ".*".toRegex()),
+    Pair("hgt", ".*".toRegex()),
+    Pair("hcl", ".*".toRegex()),
+    Pair("ecl", ".*".toRegex()),
+    Pair("pid", ".*".toRegex()),
+)
+
+val part2fieldRegexes = listOf<Pair<String, Regex>>(
+    Pair("byr", "^((19)[2-9][0-9]|(200)[0-2])\$".toRegex()),
+    Pair("iyr", "^(20)(1[0-9]|20)\$".toRegex()),
+    Pair("eyr", "^(20)(2[0-9]|30)\$".toRegex()),
+    Pair("hgt", "^(1(([5-8][0-9])|(9[0-3]))(cm))|(((59)|(6[0-9])|(7[0-6]))(in))\$".toRegex()),
+    Pair("hcl", "^#[0-9a-f]{6}\$".toRegex()),
+    Pair("ecl", "^(amb|blu|brn|gry|grn|hzl|oth)\$".toRegex()),
+    Pair("pid", "^[0-9]{9}\$".toRegex()),
+)
+
+fun main(args: Array<String>) {
+    println("Day 4 - Start!")
+    val passports = loadData("src/main/resources/day4.txt")
+
+    println("Part 1: ${countValidPassports(passports, part1fieldRegexes)}")
+    println("Part 2: ${countValidPassports(passports, part2fieldRegexes)}")
+}
+
+fun loadData(fileName: String): List<Map<String, String>> {
+    val list = mutableListOf<Map<String, String>>()
+    var passport = mutableMapOf<String, String>();
+    File(fileName).forEachLine {
+        if (it == "" && passport.isNotEmpty()) {
+            list.add(passport)
+            passport = mutableMapOf<String, String>();
+        } else {
+            val linePairs = parseTextIntoKeyValuePairs(it)
+            linePairs.forEach{ pair -> passport[pair.first] = pair.second}
+        }
+    }
+    list.add(passport)
+    return list
+}
+
+fun parseTextIntoKeyValuePairs(text: String): List<Pair<String, String>> {
+    val keyValuePairs = mutableListOf<Pair<String, String>>()
+    val keyValueTokens = text.split(" ")
+    for (keyValueToken in keyValueTokens) {
+        val tokens = keyValueToken.split(":")
+        if (tokens.count() != 2) {
+            throw Error("Could not parse text into key-value token");
+        }
+        keyValuePairs.add(Pair(tokens[0], tokens[1]))
+    }
+    return keyValuePairs
+}
+
+fun countValidPassports(passports: List<Map<String, String>>, fieldRegex: List<Pair<String, Regex>>): Int {
+    var validPassports = 0;
+    for (passport in passports) {
+        var passportIsValid = true
+        for (fieldRegex in fieldRegex) {
+            if (!passport.containsKey(fieldRegex.first) || !passport.getValue(fieldRegex.first)
+                    .matches(fieldRegex.second)
+            ) {
+                passportIsValid = false
+                break;
+            }
+        }
+        if (passportIsValid) {
+            validPassports++
+        }
+    }
+    return validPassports
+}
---
+++ src/test/kotlin/day4/Day4KtTest.kt
@@ -0,0 +1,136 @@
+package day4
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+internal class Day4KtTest {
+
+    @Test
+    fun countValidPassports() {
+        val part1Passports = listOf<Map<String, String>>(
+            mapOf<String, String>(
+                Pair("ecl", "gry"),
+                Pair("pid", "860033327"),
+                Pair("eyr", "2020"),
+                Pair("hcl", "#fffffd"),
+                Pair("byr", "1937"),
+                Pair("iyr", "2017"),
+                Pair("cid", "147"),
+                Pair("hgt", "183cm")
+            ),
+            mapOf<String, String>(
+                Pair("ecl", "amb"),
+                Pair("pid", "028048884"),
+                Pair("eyr", "2023"),
+                Pair("hcl", "#cfa07d"),
+                Pair("byr", "1929"),
+                Pair("iyr", "2013"),
+                Pair("cid", "350")
+            ),
+            mapOf<String, String>(
+                Pair("ecl", "brn"),
+                Pair("pid", "760753108"),
+                Pair("eyr", "2024"),
+                Pair("hcl", "#ae17e1"),
+                Pair("byr", "1931"),
+                Pair("iyr", "2013"),
+                Pair("hgt", "179cm")
+            ),
+            mapOf<String, String>(
+                Pair("ecl", "brn"),
+                Pair("pid", "166559648"),
+                Pair("eyr", "2025"),
+                Pair("hcl", "#cfa07d"),
+                Pair("iyr", "2011"),
+                Pair("hgt", "59in")
+            )
+        )
+
+        val part2validPassports = listOf<Map<String, String>>(
+            mapOf<String, String>(
+                Pair("pid", "087499704"),
+                Pair("hgt", "74in"),
+                Pair("ecl", "grn"),
+                Pair("iyr", "2012"),
+                Pair("eyr", "2030"),
+                Pair("byr", "1980"),
+                Pair("hcl", "#623a2f"),
+            ),
+            mapOf<String, String>(
+                Pair("pid", "896056539"),
+                Pair("hgt", "165cm"),
+                Pair("ecl", "blu"),
+                Pair("iyr", "2014"),
+                Pair("eyr", "2029"),
+                Pair("byr", "1989"),
+                Pair("hcl", "#a97842"),
+                Pair("cid", "129"),
+            ),
+            mapOf<String, String>(
+                Pair("pid", "545766238"),
+                Pair("hgt", "164cm"),
+                Pair("ecl", "hzl"),
+                Pair("iyr", "2015"),
+                Pair("eyr", "2022"),
+                Pair("byr", "2001"),
+                Pair("hcl", "#888785"),
+                Pair("cid", "88"),
+            ),
+            mapOf<String, String>(
+                Pair("pid", "093154719"),
+                Pair("hgt", "158cm"),
+                Pair("ecl", "blu"),
+                Pair("iyr", "2010"),
+                Pair("eyr", "2021"),
+                Pair("byr", "1944"),
+                Pair("hcl", "#b6652a"),
+                Pair("cid", "88"),
+            ),
+        )
+
+        val part2invalidPassports = listOf<Map<String, String>>(
+            mapOf<String, String>(
+                Pair("eyr", "1972"),
+                Pair("cid", "100"),
+                Pair("pid", "186cm"),
+                Pair("hgt", "170"),
+                Pair("ecl", "amb"),
+                Pair("iyr", "2018"),
+                Pair("byr", "1926"),
+                Pair("hcl", "#18171d"),
+            ),
+            mapOf<String, String>(
+                Pair("eyr", "1967"),
+                Pair("pid", "012533040"),
+                Pair("hgt", "170cm"),
+                Pair("ecl", "grn"),
+                Pair("iyr", "2019"),
+                Pair("byr", "1946"),
+                Pair("hcl", "#602927"),
+            ),
+            mapOf<String, String>(
+                Pair("eyr", "2020"),
+                Pair("cid", "277"),
+                Pair("pid", "021572410"),
+                Pair("hgt", "182cm"),
+                Pair("ecl", "brn"),
+                Pair("iyr", "2012"),
+                Pair("byr", "1992"),
+                Pair("hcl", "dab227"),
+            ),
+            mapOf<String, String>(
+                Pair("eyr", "2038"),
+                Pair("pid", "3556412378"),
+                Pair("hgt", "59cm"),
+                Pair("ecl", "zzz"),
+                Pair("iyr", "2023"),
+                Pair("byr", "2007"),
+                Pair("hcl", "74454a"),
+            ),
+        )
+        assertEquals(countValidPassports(part1Passports, part1fieldRegexes), 2);
+        assertEquals(countValidPassports(part2validPassports, part2fieldRegexes), part2validPassports.count());
+        assertEquals(countValidPassports(part2invalidPassports, part2fieldRegexes), 0);
+    }
+}
